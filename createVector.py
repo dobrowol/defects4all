72e965f89c9572842ef198338aecaa0b603c4fc7
dobrowol <wojdob@gmail.com>
2021-10-28 18:59:36 +0300

    adding argaprse to lcs.py and createVector.py

--- createVector.py
+++ createVector.py
@@ -3,21 +3,40 @@ import os
 train_directory = "./parsed_logs/issue1/train/"
 test_directory = "./parsed_logs/issue1/test/"
 
+import argparse
 
-def create_vector_representation(dir):
-    for filename in os.listdir(dir):
+parser = argparse.ArgumentParser(
+    description='fastText helper tool to reduce model dimensions.')
+parser.add_argument("train_directory", type=str,
+    help="directory with basic sequences")
+parser.add_argument("test_directory", type=str,
+    help="directory with sequence to be analyzed")
+
+args = parser.parse_args()
+
+if args.train_directory:
+    train_directory = args.train_directory
+if args.test_directory:
+    test_directory = args.test_directory
+
+
+def create_vector_representation(directory):
+    print ("create vec representation ", directory)
+    for filename in os.listdir(directory):
         vec_lines = []
         i=0
-        if filename.endswith(".log"):
-            with open(dir  + filename) as f:
+        if filename.endswith(".drain"):
+            with open(directory  + filename) as f:
                 lines = f.readlines()
             for line in lines:
                 vec_lines.append(line.split('[')[0])
                 i = i + 1
-            out_dir = dir + "result"
+            if directory[-1] != '/':
+                directory.append('/')
+            out_dir = directory + "vector"
             if not os.path.exists(out_dir):
                 os.mkdir(out_dir)
-            vec_path = out_dir +"/" + filename + ".vec"
+            vec_path = out_dir +"/" + filename.split('.')[0] + ".vec"
             with open(vec_path, "a+") as f:
                 for item in vec_lines:
                     if item != "None":
--- lcs.py
+++ lcs.py
@@ -2,6 +2,36 @@ train_directory = "./parsed_logs/issue1/train/result"
 test_directory = "./parsed_logs/issue1/test/result"
 import os
 from itertools import islice
+import argparse
+from createVector import create_vector_representation
+
+parser = argparse.ArgumentParser(
+    description='fastText helper tool to reduce model dimensions.')
+parser.add_argument("train_directory", type=str,
+    help="directory with basic sequences")
+parser.add_argument("test_directory", type=str,
+    help="directory with sequence to be analyzed")
+
+args = parser.parse_args()
+
+if args.train_directory:
+    train_directory = args.train_directory
+if args.test_directory:
+    test_directory = args.test_directory
+
+if test_directory[-1] != '/':
+    test_directory.append('/')
+if train_directory[-1] != '/':
+    train_directory.append('/')
+
+vec_test_dir = test_directory + "vector"
+vec_train_dir = train_directory + "vector"
+
+if not os.path.exists(vec_test_dir):
+    create_vector_representation(test_directory)
+if not os.path.exists(vec_train_dir):
+    create_vector_representation(train_directory)
+
 
 def take(n, iterable):
     "Return first n items of the iterable as a list"
@@ -12,11 +42,10 @@ similarities = {}
 
 def find_similar_sequences(line_nbr, sequence):
     similarities_at_line = {}
-    for filename in os.listdir(train_directory):
+    for filename in os.listdir(vec_train_dir):
 
         if filename.endswith(".vec"):
-            print("processing sequence ", filename)
-            with open(train_directory +"/"+ filename) as subsequence:
+            with open(vec_train_dir +"/"+ filename) as subsequence:
                 lines = subsequence.readlines()
             subsequence_id = filename.split('.')[0]
             c = 1
@@ -36,33 +65,34 @@ def find_similar_sequences(line_nbr, sequence):
                 sequence_line = sequence_line + 1
                 subseq_line_nbr = subseq_line_nbr + 1
 
-            if similarity > 0:
-                print("adding subsequence ", subsequence_id, " with similarity ", similarity)
-                similarities_at_line[subsequence_id] = similarity
+            if similarity/len(lines) > 0:
+                #print("adding subsequence ", subsequence_id, " with similarity ", similarity)
+                similarities_at_line[subsequence_id] = similarity/len(lines)
     sorted_dir_items =  dict(sorted(similarities_at_line.items(), key=lambda item: item[1], reverse=True)).items()
-    print("similar item ", list(sorted_dir_items)[:3])
     return list(sorted_dir_items)[:3]
 
 
 def sequence_length(sequence_id):
-    with open(train_directory +"/"+ sequence_id + ".log.vec") as file:
+    filename = vec_train_dir +"/"+ sequence_id + ".vec"
+    with open(filename) as file:
         lines = file.readlines()
     return len(lines)
 
 
-for filename in os.listdir(test_directory):
+for filename in os.listdir(vec_test_dir):
     if filename.endswith(".vec") :
-        print('processing ', filename)
-        with open(test_directory +"/"+ filename) as sequence:
+        with open(vec_test_dir +"/"+ filename) as sequence:
             lines = sequence.readlines()
             line_nbr = 0
             while line_nbr < len(lines):
                 similarities [line_nbr] = find_similar_sequences(line_nbr, lines)
                 if similarities [line_nbr]:
-                    line_nbr = line_nbr + sequence_length(similarities [line_nbr][0][0])
-                else:
-                    line_nbr = line_nbr + 1
-                print(line_nbr)
+                    seq_len = sequence_length(similarities [line_nbr][0][0])
+                    #print ("at line ", line_nbr, " most similar ", similarities [line_nbr][0][0], " of length ", seq_len)
+                    #line_nbr = line_nbr + sequence_length(similarities [line_nbr][0][0])
+                #else:
+                line_nbr = line_nbr + 1
+                #print (line_nbr)
 
 
 for similar_sequence in similarities.items():
