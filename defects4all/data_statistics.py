a7081f857e96d0f7463c3af46e4f525bcf564ee9
dobrowol <wojdob@gmail.com>
2021-12-13 20:57:20 +0200

    testing statistics for words which are taken from sentence of len 1

--- defects4all/data_statistics.py
+++ defects4all/data_statistics.py
@@ -2,21 +2,22 @@ import pandas as pd
 from pathlib import Path
 
 def describe_datasets(input_dir):
+    for path in Path(input_dir).rglob('klog*/sentence1/*.klog'):
+        print("word statistics for ", path)
+        df = pd.read_csv(path, sep='\t',names= ["test_suite", "klogs"])
+        print(df.klogs.value_counts())
     for path in Path(input_dir).rglob('*.klog'):
         print(path)
-        f= open(path,"r")
-        klogs_list = {"klogs": []}
-        for line in f.readlines():
-            print(line)
-            klogs = line.split('\t')[1]
-            klogs_list["klogs"].extend(klogs.split())
-        f.close()
-        dataset = pd.DataFrame(klogs_list, columns=["klogs"])
+        #for line in f.readlines():
+        #    print(line)
+        #    klogs = line.split('\t')[1]
+        #    klogs_list["klogs"].extend(klogs.split())
+        dataset = pd.read_csv(path, sep='\t',names=["test_suite", "klogs"])
         
-        print ("describing ", path)
+        print ("balance of dataset", path)
         #dataset = pd.read_csv(path, sep='\t')
         #pd.dataset.iloc[:, 2]
-        print(dataset.klogs.value_counts())
+        print(dataset.test_suite.value_counts(normalize=True))
         print(dataset.describe())
 
 #describe_datasets("./klogs")
